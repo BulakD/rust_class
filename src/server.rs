dae7709d1632c274dc58a0f90e13fc1938dfa49f
Dmitry <dmitryi@proton.me>
2024-04-30 20:41:37 +0300

    Initial commit

---
+++ src/server.rs
@@ -0,0 +1,16 @@
+use std::net::TcpListener;
+pub  struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub   fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    pub   fn run(self) {
+        println!("Server listening on {}", self.addr);
+
+        let listener = TcpListener::bind((&self.addr)).unwrap();
+    }
+}
